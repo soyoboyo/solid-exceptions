fce80b92bb9e632e0ced0d30e7c5b473be831461
Jacek Zalewski <jacekzalewski95@gmail.com>
2022-05-18 14:29:04 +0200

    Migrate to JUnit 5 parmetrized tests

--- src/test/java/com/pany/exceptions/ExceptionCreatorTest.java
+++ src/test/java/com/pany/exceptions/ExceptionCreatorTest.java
@@ -1,46 +1,34 @@
 package com.pany.exceptions;
 
-import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-@RunWith(JUnitParamsRunner.class)
+import java.util.stream.Stream;
+
 public class ExceptionCreatorTest {
 
-	@Test
-	@Parameters(method = "provideTestCases")
-	public void shouldCreateException(TestCase testCase) {
+	@ParameterizedTest
+	@MethodSource("provideTestCases")
+	public void shouldCreateException(String pageSource, Exception expectedException) {
 		//given
 		ExceptionCreator objectUnderTest = new ExceptionCreator();
 
 		//when
-		PacerCrawlerException result = objectUnderTest.createException(testCase.pageSource);
+		PacerCrawlerException result = objectUnderTest.createException(pageSource);
 
 		//then
-		Assertions.assertThat(result).isInstanceOf(testCase.expectedException.getClass());
-		Assertions.assertThat(result.getMessage()).isEqualTo(testCase.expectedException.getMessage());
+		Assertions.assertThat(result).isInstanceOf(expectedException.getClass());
+		Assertions.assertThat(result.getMessage()).isEqualTo(expectedException.getMessage());
 	}
 
-	TestCase[] provideTestCases() {
-		//@formatter:off
-		return new TestCase[]{
-				new TestCase("Invalid username or password is at the page", new InvalidPacerCredentialsException("Invalid Pacer Credentials", "Invalid username or password is at the page")),
-				new TestCase("Invalid ID or password is at the page", new InvalidPacerCredentialsException("Invalid Pacer Credentials", "Invalid ID or password is at the page")),
-				new TestCase("Pacer filing account is at the page", new PacerFillingAccountException("Pacer Filing account", "Pacer filing account is at the page"))
-		};
-		//@formatter:on
-	}
-
-	private final static class TestCase {
-		private final String pageSource;
-		private final Exception expectedException;
-
-		private TestCase(String pageSource, Exception expectedException) {
-			this.pageSource = pageSource;
-			this.expectedException = expectedException;
-		}
+	private static Stream<Arguments> provideTestCases() {
+		return Stream.of(
+				Arguments.of("Invalid username or password is at the page", new InvalidPacerCredentialsException("Invalid Pacer Credentials", "Invalid username or password is at the page")),
+				Arguments.of("Invalid ID or password is at the page", new InvalidPacerCredentialsException("Invalid Pacer Credentials", "Invalid ID or password is at the page")),
+				Arguments.of("Pacer filing account is at the page", new PacerFillingAccountException("Pacer Filing account", "Pacer filing account is at the page"))
+		);
 	}
 
 }
